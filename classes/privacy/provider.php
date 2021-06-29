7543833d15f211bbc353922856d16305cfcb44b8
Robert Schrenk <robert@schrenk.cc>
2021-06-29 11:09:39 +0200

    implement privacy api

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -17,17 +17,110 @@
 /**
  * @package    local_webuntis
  * @copyright  2021 Zentrum für Lernmanagement (www.lernmanagement.at)
- * @author    Robert Schrenk
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Robert Schrenk
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 namespace local_webuntis\privacy;
 use core_privacy\local\metadata\collection;
+use \core_privacy\local\request\writer;
 
 defined('MOODLE_INTERNAL') || die;
 
-class provider implements \core_privacy\local\metadata\null_provider {
-    public static function get_reason() : string {
-        return 'privacy:metadata';
+class provider implements
+                    \core_privacy\local\metadata\provider,
+                    \core_privacy\local\request\core_userlist_provider,
+                    \core_privacy\local\request\plugin\provider {
+
+    public static function get_metadata(collection $collection) : collection {
+        $collection->add_database_table(
+            'local_webuntis_usermap',
+            [
+                'tenant_id' => 'privacy:metadata:local_webuntis_usermap:tenant_id',
+                'school' => 'privacy:metadata:local_webuntis_usermap:school',
+                'remoteuserid' => 'privacy:metadata:local_webuntis_usermap:remoteuserid',
+                'remoteuserrole' => 'privacy:metadata:local_webuntis_usermap:remoteuserrole',
+                'userid' => 'privacy:metadata:local_webuntis_usermap:userid',
+                'timecreated' => 'privacy:metadata:local_webuntis_usermap:timecreated',
+                'lastaccess' => 'privacy:metadata:local_webuntis_usermap:lastaccess',
+                'firstname' => 'privacy:metadata:local_webuntis_usermap:firstname',
+                'lastname' => 'privacy:metadata:local_webuntis_usermap:lastname',
+                'email' => 'privacy:metadata:local_webuntis_usermap:email',
+                'userinfo' => 'privacy:metadata:local_webuntis_usermap:userinfo',
+                'candisconnect' => 'privacy:metadata:local_webuntis_usermap:candisconnect',
+            ],
+            'privacy:metadata:local_webuntis_usermap'
+        );
+        return $collection;
     }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param   approved_userlist       $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist) {
+        global $DB;
+        $userids = $userlist->get_userids();
+        list($userinsql, $userinparams) = $DB->get_in_or_equal($userlist->get_userids(), SQL_PARAMS_NAMED);
+        $sql = "userid {$userinsql}";
+        $DB->delete_records_select('local_webuntis_usermap', $sql, $userinparams);
+    }
+    /**
+     * Delete all personal data for all users in the specified context.
+     *
+     * @param context $context Context to delete data from.
+     */
+    public static function delete_data_for_all_users_in_context(\context $context) {
+        // Data is only stored in global context.
+    }
+    public static function delete_data_for_user(approved_contextlist $contextlist) {
+        global $DB;
+        $userid = $contextlist->get_user()->id;
+        $DB->delete_records('local_webuntis_usermap', [ 'userid' => $userid ]);
+    }
+    /**
+     * Export all user data for the specified user, in the specified contexts, using the supplied exporter instance.
+     *
+     * @param   approved_contextlist    $contextlist    The approved contexts to export information for.
+     */
+    public static function export_user_data(approved_contextlist $contextlist) {
+        global $DB;
+        $userid = $contextlist->get_user()->id;
+        $context = \context_system::instance();
+        $mappings = $DB->get_records('local_webuntis_usermap', [ 'userid' => $userid ]);
+        foreach ($mappings as $mapping) {
+            writer::with_context($context)
+                ->export_data([get_string('pluginname', 'local_webuntis'),get_string('usermapping','local_webuntis')], $mapping);
+        }
+    }
+    /**
+     * Get the list of contexts that contain user information for the specified user.
+     *
+     * @param   int           $userid       The user to search.
+     * @return  contextlist   $contextlist  The list of contexts used in this plugin.
+     */
+    public static function get_contexts_for_userid(int $userid) : contextlist {
+        // Data is only stored in global context.
+        $contextlist = new \core_privacy\local\request\contextlist();
+        return $contextlist;
+    }
+    /**
+     * Get the list of users who have data within a context.
+     *
+     * @param   userlist    $userlist   The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist) {
+        // Data is only stored in global context.
+        $context = $userlist->get_context();
+
+        if (!$context instanceof \context_system) {
+            return;
+        }
+        $sql = "SELECT userid FROM {local_webuntis_usermap}";
+        $userlist->add_from_sql('userid', $sql, []);
+    }
+
+
+
 }
